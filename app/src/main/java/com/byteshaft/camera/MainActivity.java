e9d1859621f0d02b2dacc595f518371a3d00139b
Omer Akram <omer.akram@crossbario.com>
2017-10-31 12:31:33 +0500

    Add photo detector WAMP

--- app/src/main/java/com/byteshaft/camera/MainActivity.java
+++ app/src/main/java/com/byteshaft/camera/MainActivity.java
@@ -5,8 +5,16 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.ImageButton;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import java.io.ByteArrayOutputStream;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import io.crossbar.autobahn.wamp.Client;
+import io.crossbar.autobahn.wamp.Session;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,13 +26,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageButton = findViewById(R.id.image_button);
-
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, CAMERA_REQUEST);
-            }
+        imageButton.setOnClickListener(v -> {
+            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraIntent, CAMERA_REQUEST);
         });
     }
 
@@ -32,6 +36,24 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
             imageButton.setImageBitmap(photo);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            photo.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            byte[] byteArray = stream.toByteArray();
+            Session session = new Session();
+            session.addOnJoinListener((session1, details) -> {
+                CompletableFuture<List<Object>> res = session1.call(
+                        "io.crossbar.detect_faces", new TypeReference<List<Object>>() {}, null,
+                        byteArray);
+                res.whenComplete((cords, throwable) -> {
+                    if (throwable != null) {
+                        throwable.printStackTrace();
+                    } else {
+                        System.out.println(cords);
+                    }
+                });
+            });
+            Client client = new Client(session, "ws://192.168.1.9:8080/ws", "realm1");
+            client.connect().whenComplete((exitInfo, throwable) -> System.out.println(exitInfo));
         }
     }
 }
